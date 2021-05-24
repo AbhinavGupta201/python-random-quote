49ba08816c2c638dcd74a8b06115dc29d8daf01f
Abhinav <guptaabhinav707@gmail.com>
2021-05-24 18:34:25 +0530

    added random quote generator logic

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,20 @@
+import random
+
+
 def primary():
-  print("Keep it logically awesome.")
+    # print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+    f = open("quotes.txt")
+    quotes = f.readlines()
+    f.close()
 
-  #print(quotes)
+    last=len(quotes)
 
-if __name__== "__main__":
-  primary()
+    rnd = random.randint(0, last)
+
+
+    print(quotes[rnd])
+
+
+if __name__ == "__main__":
+    primary()
